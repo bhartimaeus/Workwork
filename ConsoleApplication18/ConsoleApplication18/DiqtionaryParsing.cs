ae2157f42255fe0d10cdb0b15170c07e527435c3
Miron <miron.leanca@gmail.com>
2017-06-21 23:35:38 +0100

    Added dictionary json and MironUtilBox

---
+++ ConsoleApplication18/ConsoleApplication18/DiqtionaryParsing.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace MironsUtilBox
+{
+    public class ParseUtils
+    {
+        public static bool Diqtionary(string filename, out Dictionary<string, string> DiqOut)
+        {
+            var allText = File.ReadAllText(filename);
+            char[] characters = allText.ToCharArray();
+            DiqOut = new Dictionary<string, string>();
+            bool isdonereading = false;
+            int currIndex = 0;
+            string denominator = "";
+            string description = "";
+            try
+            {
+                while (!isdonereading)
+                {
+                    if (characters[currIndex] == '{')
+                    {
+                        currIndex++;
+                    }
+
+                    if (characters[currIndex] == '"')
+                    {
+                        currIndex++;
+
+                        while (characters[currIndex] != '"')
+                        {
+                            denominator += characters[currIndex];
+                            currIndex++;
+                        }
+                    }
+
+                    currIndex++;
+
+                    if (characters[currIndex] == ':')
+                    {
+                        currIndex++;
+
+                        if (characters[currIndex] == '"')
+                        {
+                            currIndex++;
+
+                            while (characters[currIndex] != '"')
+                            {
+                                description += characters[currIndex];
+                                currIndex++;
+                            }
+                        }
+                    }
+
+                    currIndex++;
+
+                    if (!DiqOut.ContainsKey(denominator))
+                    {
+                        DiqOut.Add(denominator, description);
+                    }
+
+                    if (characters[currIndex] == '}')
+                    {
+                        isdonereading = true;
+                    }
+                    description = "";
+                    denominator = "";
+                    currIndex++;
+                    if (currIndex == characters.Length)
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+            return true;
+
+        }
+
+        public static List<char> AllowedChars_English = "abcdefghijklmnopqrstuvwxyz".ToCharArray().ToList();
+            
+        public static bool ValidateWord(string word, List<char> allowedChars)
+        {
+            char[] chars = word.ToCharArray();
+            foreach (var c in chars)
+            {
+                if (!allowedChars.Contains(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
---
+++ ConsoleApplication18/Testing/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MironsUtilBox;
+
+namespace Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> TesterDiq;
+            if (ParseUtils.Diqtionary(@"../dictionary.json.txt", out TesterDiq))
+            {
+                try
+                {
+
+               
+                Console.WriteLine(TesterDiq.Keys.ElementAt(1000).ToString());
+                Console.WriteLine(TesterDiq.Keys.ElementAt(100).ToString());
+                Console.WriteLine(TesterDiq.Keys.ElementAt(100).ToString());
+                Console.WriteLine(TesterDiq.Keys.ElementAt(TesterDiq.Count-1).ToString());
+                Console.ReadLine();
+                }
+                catch (Exception e)
+                {
+
+                    Console.WriteLine(e.ToString());
+                    Console.ReadLine();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Something don gone fucked up son.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
